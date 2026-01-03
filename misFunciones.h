58fcf0fe3f5671fe2c7722c970938c878935caec
Adrian <adrian.cadena.cardenas@gmail.com>
2026-01-02 18:18:42 -0600

    funciones de separacion y despedida añadidas

---
+++ misFunciones.h
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+void portada() {
+    printf("******************************************************\n");
+    printf("*                                                    *\n");
+    printf("*              Proyecto de Ejemplo C                 *\n");
+    printf("*            Autor: Adrian Cadena                    *\n");
+    printf("*                                                    *\n");
+    printf("******************************************************\n");
+}   
+
+void separaciones() {
+    printf("------------------------------------------------------\n");
+    printf("------------------------------------------------------\n");
+}
+
+void despedida() {
+    printf("******************************************************\n");
+    printf("*            Gracias por usar el programa!           *\n");
+    printf("******************************************************\n");
+}
---
+++ proyecto2.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include "misFunciones.h"
+
+int main() {
+
+    portada();
+    separaciones();
+    despedida();
+    return 0;
+    system("pause");    
+}
